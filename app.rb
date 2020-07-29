ec858a8c84a0120ad09b4926a3ad4c651916312b
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2020-07-29 15:51:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
+<<<<<<< HEAD
 		 erb :index
 	end
 
@@ -10,3 +11,9 @@ class App < Sinatra::Base
 		erb :info
 	  end
 end
+=======
+    #  erb :index
+    
+	end
+end
+>>>>>>> d0eea9f61555ddd03202fc15719226c130f8bc39
